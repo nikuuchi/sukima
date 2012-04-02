cc58477831880941caad7e2e014c301ca74a6c0c
uchida <uchida.atsushi.8@gmail.com>
2012-04-02 18:48:02 +0900

    Convert Space to Tab.

--- lisp.c
+++ lisp.c
@@ -12,13 +12,13 @@
 typedef enum Type {TY_Car,TY_Cdr,TY_Op,TY_Value,TY_Nil,TY_Name} Type;
 
 typedef struct cons_t{
-  Type type;
-  union {
-    struct cons_t *car;
-    int ivalue;
-    char *svalue;
-  };
-  struct cons_t *cdr;
+	Type type;
+	union {
+		struct cons_t *car;
+		int ivalue;
+		char *svalue;
+	};
+	struct cons_t *cdr;
 } cons_t;
 
 
@@ -29,27 +29,27 @@ void Cons_init(cons_t *Cell){
 
 int main(int argc, char *argv[])
 {
-  if(argc == 1){
-    //REPL
-  }else if(argc == 2){
-    FILE *fp;
-    char *buf;
-    buf = (char *)malloc(sizeof(char) * BUF_SIZE);
-    if((fp=fopen(argv[1],"r")) == NULL){
-      printf("file open error\n");
-      exit(0);
-    }
+	if(argc == 1){
+		//REPL
+	}else if(argc == 2){
+		FILE *fp;
+		char *buf;
+		buf = (char *)malloc(sizeof(char) * BUF_SIZE);
+		if((fp=fopen(argv[1],"r")) == NULL){
+			printf("file open error\n");
+			exit(0);
+		}
 
-    while(fgets(buf,BUF_SIZE,fp)){
-      printf("%s",buf);
-    }
+		while(fgets(buf,BUF_SIZE,fp)){
+			printf("%s",buf);
+		}
 
-      fclose(fp);
-  }else{
-    printf("Too many arguments.");
-  }
+		fclose(fp);
+	}else{
+		printf("Too many arguments.");
+	}
   
-  return 0;
+	return 0;
 }
 
 
